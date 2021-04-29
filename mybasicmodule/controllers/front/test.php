1f570330114ae715667ba35575900606956a525d
ALOUI Mohamed Habib <alouimohamedhabib@yahoo.com>
2021-04-30 00:02:34 +0200

    Add test front controller.

--- mybasicmodule/controllers/front/test.php
+++ mybasicmodule/controllers/front/test.php
@@ -3,4 +3,11 @@
 
 class MyBasicModuleTestModuleFrontController extends ModuleFrontController
 {
+    public   $template = "module:mybasicmodule/views/templates/front/test.tpl";
+
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate($this->template);
+    }
 }
