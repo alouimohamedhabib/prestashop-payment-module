f585651559fac1ba2c07574b5b7bca91a09d3e73
ALOUI Mohamed Habib <alouimohamedhabib@yahoo.com>
2021-06-17 02:16:50 +0200

    Add twig template

--- mybasicmodule/src/controller/commentController.php
+++ mybasicmodule/src/controller/commentController.php
@@ -2,12 +2,13 @@
 
 namespace Mybasicmodule\Controller;
 
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Response;
 
-class CommentController
+class CommentController extends FrameworkBundleAdminController
 {
     public function indexAction()
     {
-        return new Response("Hello world");
+        return $this->render("@Modules/mybasicmodule/views/templates/admin/comment.html.twig");
     }
 }
