28490f3ca3a263b944646a818a9c61bd8dc1dab6
ALOUI Mohamed Habib <alouimohamedhabib@yahoo.com>
2021-07-12 01:00:46 +0200

    Add form submission handling.

--- mybasicmodule/src/controller/commentController.php
+++ mybasicmodule/src/controller/commentController.php
@@ -5,12 +5,18 @@ namespace Mybasicmodule\Controller;
 use Mybasicmodule\Form\CommentType;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends FrameworkBundleAdminController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $form = $this->createForm(CommentType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());
+        }
 
         return $this->render(
             "@Modules/mybasicmodule/views/templates/admin/comment.html.twig",
