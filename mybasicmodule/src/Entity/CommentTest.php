77dc7a8dc7f2f20dcc1848e8196e8747a5d54b23
ALOUI Mohamed Habib <alouimohamedhabib@yahoo.com>
2021-07-16 01:14:57 +0200

    Create the first entity

---
+++ mybasicmodule/src/Entity/CommentTest.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Mybasicmodule\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class CommentTest
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+}
